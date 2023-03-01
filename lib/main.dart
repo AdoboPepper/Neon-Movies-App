57644d3dc171ac64a8bb01bbc076210576893197
AdoboPepper <jasper.calimbo@usjr.edu.ph>
2023-03-02 00:38:27 +0800

    Adds Eternal detail screen and spiderman detail screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:moviesapp/screens/eternals_movie_details.dart';
 import 'package:moviesapp/screens/home_screen.dart';
+import 'package:moviesapp/screens/movie_detail_screen.dart';
 import 'package:moviesapp/screens/onboarding_screen.dart';
+import 'package:moviesapp/screens/spiderman_movie_details.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,12 +16,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: GoogleFonts.openSans().fontFamily,
       ),
-       home: const HomeScreen(),
+
+      //home: const DetailsEternals(),
+      home: const SpidermanDetails(),
+      //home: const HomeScreen(),
       //home: const OnboardingScreen(),
     );
   }
